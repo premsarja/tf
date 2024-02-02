d13382f171e3a10b2f2794cf3dd644df5fc37333
premsarja <premsagar@prodot.in>
2024-02-02 15:38:07 +0530

    done

---
+++ security-group/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket   = "sagare"
+    key      = "dev/ec2/terraform.tfstate"
+    region   = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+
+  region  = "us-east-1"
+
+
+}

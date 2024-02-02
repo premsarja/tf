afef14a1d070a5b73f2a99c73572bdae3677edcb
premsarja <premsagar@prodot.in>
2024-02-02 09:47:03 +0530

    done

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,26 +1,4 @@
 
-terraform {
-
-  required_providers {
-
-    aws = {
-
-      source  = "hashicorp/aws"
-
-      version = "~> 4.16"
-
-    }
-
-  }
-
-
-
-  required_version = ">= 1.2.0"
-
-}
-
-
-
 provider "aws" {
 
   region  = "us-west-2"

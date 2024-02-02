a9fee6ce0b045607471ba3caa8b7c595b62d1aad
premsarja <premsagar@prodot.in>
2024-02-02 17:04:12 +0530

    done

---
+++ modules/ec2/main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "sagar" {
+  name        = "allow-sagar"
+  description = "Allow TLS inbound traffics"
+
+  ingress {
+    description = "ssh  from VPC internet "
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_sagar"
+  }
+}
+
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "^ami-terraform"
+  owners           = ["self"]
+}
+
+
+resource "aws_instance" "ami" {
+  ami             = data.aws_ami.ami.id # Replace with your desired AMI ID"
+  instance_type   = "t2.micro"
+#   key_name        = "your-key-pair-name"     # Replace with your key pair name
+  vpc_security_group_ids = [aws_security_group.sagar.id]
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+

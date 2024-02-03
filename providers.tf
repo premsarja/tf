897c0d08dac0c8047acc99e0b89602e9d9e82168
premsarja <premsagar@prodot.in>
2024-02-03 13:46:39 +0530

    done

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket   = "sagare"
+    key      = "current/terraform.tfstate"
+    region   = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region  = "us-east-1"
+}
